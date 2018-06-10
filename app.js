5546d7af3bac8c1daf630c3739ceccc1076b8674
Tim Rowe <tim@tjsr.id.au>
2018-06-10 11:34:10 +1000

    Initial commit - takes team list to only include those teams.

---
+++ app.js
@@ -0,0 +1,28 @@
+const owl = require('./owlcalendar');
+
+const http = require('http');
+const handler = require('serve-handler');
+var getIP = require('ipware')().get_ip;
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+function onRequest(request, response) {
+	var ipInfo = getIP(request);
+	var clientIp = ipInfo.clientIp;
+	
+	var teams = owl.getFilteredTeams(request);
+	if (teams) {
+		console.log(clientIp + " Returning only teams " + teams);
+	} else {
+		console.log(clientIp + " Returning all teams.");
+	}
+	owl.getOwlIcal(response, teams);
+}
+
+const server = http.createServer(onRequest);
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
---
+++ owlcalendar.js
@@ -0,0 +1,146 @@
+'use strict';
+
+var fs = require('fs');
+var icalgen = require('ical-generator');
+var calendarData;
+var ical = icalgen().ttl(60*60*24);
+
+
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (obj, fromIndex) {
+		if (fromIndex == null) {
+			fromIndex = 0;
+		} else if (fromIndex < 0) {
+			fromIndex = Math.max(0, this.length + fromIndex);
+		}
+		for (var i = fromIndex, j = this.length; i < j; i++) {
+		if (this[i] === obj)
+			return i;
+		}
+		return -1;
+	};
+}
+
+exports.getCachedData = function(onDataRetrieved, response, teams) {
+	if (calendarData == null) {
+		console.log("Loading calendar data.");
+		fs.readFile('calendar.json', 'utf8', function (err,data) {
+			if (err) {
+				return console.log("Error retrieving calendar.json: " + err);
+			} else {
+				console.log("Calendar data retrieved.");
+				calendarData = JSON.parse(data);
+				onDataRetrieved(calendarData, response, teams);
+			}
+		});
+	} else {
+		console.log("Calendar data already loaded.");
+		onDataRetrieved(calendarData, response);
+	}
+};
+
+function parseStageInto(stage, ical, filteredTeams) {
+	var matches = stage.matches;
+	for (var i = 0;i < matches.length;i++) {
+		parseMatchesInto(stage.name, matches[i], ical, filteredTeams);
+	}
+}
+
+function getAbbreviatedName(competitor) {
+	var abbr;
+	if (competitor == null) {
+		abbr = "TBA";
+	} else if (competitor.abbreviatedName) {
+		abbr = competitor.abbreviatedName;
+	} else if (competitor.content.abbreviatedName) {
+		abbr = competitor.content.abbreviatedName;
+	}
+	return abbr;
+}
+
+function parseMatchesInto(stageName, match, ical, filteredTeams) {
+	var competitors = match.competitors;
+	if (competitors.length < 2 || !competitors) {
+		console.log("Competitors in match was null: " + JSON.stringify(match));
+//	} else if (!competitors[0] == null || competitors[1] == null) {
+//		console.log("Competitors in match was null: " + JSON.stringify(match));
+//	} else if (!competitors[0].name || !competitors[1].name) {
+//		console.log("Competitor in match had no name: " + JSON.stringify(match));
+	} else {
+		var comp1 = competitors[0] == null ? "TBA" : competitors[0].name;
+		var comp2 = competitors[1] == null ? "TBA" : competitors[1].name;
+		var abbr1 = getAbbreviatedName(competitors[0]);
+		var abbr2 = getAbbreviatedName(competitors[1]);
+		
+		var showMatch = true;
+		if (filteredTeams == null || filteredTeams.length == 0) {
+			// filter to only show matches for teams in array.
+			showMatch = true;
+		} else {
+			if (filteredTeams.indexOf(abbr1) !== -1 || filteredTeams.indexOf(abbr2) !== -1) {
+				showMatch = true;
+			}
+			else {
+				showMatch = false;
+			}
+		}
+		
+		if (showMatch) {
+			var description = stageName;
+			var summary = "OWL ";
+			if (match.tournament.type == 'PLAYOFFS') {
+				description += ' Playoffs';
+			}
+			description += " - " + comp1 + " vs " + comp2;
+			summary += abbr1 + " v " + abbr2;
+			var event = ical.createEvent({
+				summary: summary,
+				description: description,
+				start: match.startDate,
+				end: match.endDate,
+				sequence: match.id,
+				location: match.tournament.location
+			});
+	
+			//console.debug("Match " + match.id + " at " + match.startDate + ": " + summary);
+		}
+	}
+}
+
+exports.getCalendar = function(calData, response, teams) {
+	var stages = calData.data.stages;
+	
+	for (var i = 0;i < stages.length;i++) {
+		parseStageInto(stages[i], ical, teams);
+	}
+
+	ical.serve(response);
+};
+
+var params=function (req) {
+  let q=req.url.split('?'),result={};
+  if(q.length>=2){
+      q[1].split('&').forEach((item)=>{
+           try {
+             result[item.split('=')[0]]=item.split('=')[1];
+           } catch (e) {
+             result[item.split('=')[0]]='';
+           }
+      })
+  }
+  return result;
+}
+
+exports.getFilteredTeams = function(request) {
+	var pars = params(request);
+	var teams = null;
+	if (pars.teams) {
+		teams = pars.teams.split(',');
+	}
+	return teams;
+}
+
+exports.getOwlIcal = function(response, teams) {
+	exports.getCachedData(exports.getCalendar, response, teams);
+};
+
